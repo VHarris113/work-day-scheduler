f3abda86d67b18460ea46eabe4544e1176be0931
Veronica Harris <veronica.n.harris92@gmail.com>
2021-03-25 19:09:52 -0400

    working on js and jquery now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Timeblock editing
+// Timeblock edits staying if page refreshed
+// Being able to save timeblock edits to page
+// Timeblock changing colors for past, present, and future
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(time);
 var update = function() {
@@ -6,3 +10,7 @@ var update = function() {
 }
 setInterval(update, 1000);
 
+var timeBlock = $('#time-block');
+var past = $('.past');
+var present = $('.present');
+var future = $('future');
