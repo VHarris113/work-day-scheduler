2ac3579026680cafee4c994d02b92fafab83e392
Veronica Harris <veronica.n.harris92@gmail.com>
2021-03-26 16:09:42 -0400

    finalized JS with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,12 +38,18 @@ function currentHour() {
                         $(this).removeClass("present");
     }
   });
-
-
-
-  $("")
-
-
+  
+  $("#9AM .description").val(localStorage.getItem("9AM"));
+  $("#10AM .description").val(localStorage.getItem("10AM"));
+  $("#11AM .description").val(localStorage.getItem("11AM"));
+  $("#12PM .description").val(localStorage.getItem("12PM"));
+  $("#1PM .description").val(localStorage.getItem("1PM"));
+  $("#2PM .description").val(localStorage.getItem("2PM"));
+  $("#3PM .description").val(localStorage.getItem("3PM"));
+  $("#4PM .description").val(localStorage.getItem("4PM"));
+  $("#5PM .description").val(localStorage.getItem("5PM"));
+  
+  currentHour();
 
 }
 
