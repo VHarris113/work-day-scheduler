f1f9372eed526025fa1e19ab074c6a0a254f1c53
Veronica Harris <veronica.n.harris92@gmail.com>
2021-03-27 15:25:45 -0400

    finalized the js now

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,27 @@ var update = function () {
 setInterval(update, 1000);
 
 $(document).ready(function() {
-    $(".saveBtn").on("click", function () {
 
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+  $(".saveBtn").on("click", function () {
+      console.log(this);
 
-        localStorage.setItem(text, time);
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
 
-        $("#9AM .description").val(localStorage.getItem("description"));
-        $("#10AM .description").val(localStorage.getItem("10AM"));
-        $("#11AM .description").val(localStorage.getItem("11AM"));
-        $("#12PM .description").val(localStorage.getItem("12PM"));
-        $("#1PM .description").val(localStorage.getItem("1PM"));
-        $("#2PM .description").val(localStorage.getItem("2PM"));
-        $("#3PM .description").val(localStorage.getItem("3PM"));
-        $("#4PM .description").val(localStorage.getItem("4PM"));
-        $("#5PM .description").val(localStorage.getItem("5PM"));
+      localStorage.getItem(time, text);
+
     })
 });
+
+$("#9AM .description").val(localStorage.getItem("#9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11AM .description").val(localStorage.getItem("11AM"));
+$("#12PM .description").val(localStorage.getItem("12PM"));
+$("#1PM .description").val(localStorage.getItem("1PM"));
+$("#2PM .description").val(localStorage.getItem("2PM"));
+$("#3PM .description").val(localStorage.getItem("3PM"));
+$("#4PM .description").val(localStorage.getItem("4PM"));
+$("#5PM .description").val(localStorage.getItem("5PM"));
 
 window.onload = function() {
   colorCode();
